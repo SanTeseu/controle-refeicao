fbf032496aa3054f99a68a1295901cd4570ae5df
SanTeseu <matheussant1805@gmail.com>
2025-10-30 19:41:02 -0300

    URL do projeto Supabasse atualizada

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 // 🔧 Cole aqui os dados do seu projeto Supabase:
-const SUPABASE_URL = 'https://SEU-PROJETO.supabase.co'
-const SUPABASE_ANON_KEY = 'SUA-CHAVE-ANON'
+const SUPABASE_URL = 'https://owsjuburmtniilafuwip.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93c2p1YnVybXRuaWlsYWZ1d2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NTA2NjMsImV4cCI6MjA3NzQyNjY2M30.F5syr9rtzoq2p_uVxfrFBa80RXcvjM4SxrxHuW-lgO8'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
